b4091ae9683796823269e84b215d79d66f008d16
vlavirus <androando777@gmail.com>
2021-03-23 14:15:47 +0200

    feat(core): add constants to the app

---
+++ src/app/constants/api.constant.ts
@@ -0,0 +1,3 @@
+export const ApiConstants = {
+  url: 'https://api.exchangeratesapi.io/latest?base=USD&symbols=GBP,AUD,RUB,JPY,CAD,CHF'
+};
--- src/app/system/currency.service.ts
+++ src/app/system/currency.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { ApiConstants } from '../constants/api.constant'
+
 @Injectable({ providedIn: 'root'})
 export class CurrencyService{
   constructor(private http: HttpClient) {}
 
   public getCurrencyList(): Observable<any> {
-    return this.http.get<{}>('https://api.exchangeratesapi.io/latest?base=USD&symbols=GBP,AUD,RUB,JPY,CAD,CHF');
+    return this.http.get<{}>(`${ApiConstants.url}`);
   }
 }
